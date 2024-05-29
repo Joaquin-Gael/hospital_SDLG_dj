75d9fcdaada2e1569369eb6c090bc04a2d7e5521
git-Joaquin-Gael <guzmanantonio867@gmail.com>
2024-05-29 13:01:52 -0300

    funca la lista de turnos

--- hospital_SDLG_dj/user_panel/urls.py
+++ hospital_SDLG_dj/user_panel/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('panel/',views.user_panel)
+    path('panel/',views.user_panel),
+    path('turnos/',views.turnos_list)
 ]
--- hospital_SDLG_dj/user_panel/views.py
+++ hospital_SDLG_dj/user_panel/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from turnero import models as turn_models
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def user_panel(request):
     return render(request,'panel.html')
+
+@login_required
+def turnos_list(request):
+    try:
+        user = request.user
+        try:
+            paciente = turn_models.Paciente.objects.get(usuario=user)
+            turnos = turn_models.Turno.objects.filter(paciente=paciente)
+            return render(request,'turnos.html',{'turnos':turnos})
+        except Exception as err:
+            print(err)
+            turnos = []
+            error_text = "Usted no tiene turnos"
+            return render(request,'turnos.html',{'turnos':turnos,'error':error_text})
+    except Exception as err:
+        print(err)
+        return redirect('home_blog')
